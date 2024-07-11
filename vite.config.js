1a192c8e0d2b996760f2a1f268a3663a57ec292d
Havyas U <113080389+HavyasRai@users.noreply.github.com>
2024-07-11 22:17:19 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios', 'react-hook-form', 'react-icons'],
+      external: ['axios', 'react-hook-form', 'react-icons/md'],
     },
   },
 });
