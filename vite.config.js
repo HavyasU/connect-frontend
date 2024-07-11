e71a15b1545b88bea35c28f55d0494e0e7e7ae58
Havyas U <113080389+HavyasRai@users.noreply.github.com>
2024-07-11 22:02:27 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['axios'],
     },
   },
-  plugins: [react()],
 });
