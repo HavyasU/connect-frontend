9ea183c9108313cfac6ad91ffd928ca05e888034
Havyas U <113080389+HavyasRai@users.noreply.github.com>
2024-08-26 21:39:24 +0530

    commit.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { TbSocial } from "react-icons/tb";
 import { useForm } from "react-hook-form";
